a78b6f1383bd398c63cc0561c0ebc39dc0fee9f5
Hung Tran <tranhungt1@gmail.com>
2014-09-23 20:41:17 -0700

    adds a function to populate db

--- demo.js
+++ demo.js
@@ -6,7 +6,14 @@ var id = table.insert({
     lname: 'Tran'
 });
 
-var obj1 = table.findByAttribute('lname', 'Tran');
+for(var i = 0; i < 2000; i++){
+    table.insert({
+        fname: 'Hung' + i,
+        lname: 'Tran' + i
+    })
+}
+
+var obj1 = table.findByAttribute('lname', 'Tran100');
 var obj2 = table.findById(id);
 
 console.log(obj1);
